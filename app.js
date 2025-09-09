dcea2924b729cb16534317ff2c9132a9f53fe8a1
zieir0 <107752516+zieir0@users.noreply.github.com>
2025-09-09 04:14:38 -0300

    testing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 15;
+let numeroLimite = 20;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto, caramba que *** enorme');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 15');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 20');
 }
 
 exibirMensagemInicial();
